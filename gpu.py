f001e520c02b3e0ffee1f22bf17133732ab3f0be
soniavaidya05 <sonia.vaidya05@gmail.com>
2025-06-16 17:00:58 -0400

    Basic gpu class

---
+++ gpu.py
@@ -0,0 +1,14 @@
+class GPU:
+    def __init__(self, id):
+        self.id = id
+        self.current_job = None 
+        self.heat = 0.0 # accumulated thermal energy in gpu
+        self.cooling_rate = 0.1 # TODO: Find a realistic value
+        self.max_temp = 100
+
+    def step(self):
+        if self.current_job:
+            self.heat += self.current_job.thermal_output 
+        else:
+            self.heat -= self.cooling_rate
+            self.heat = max(self.heat, 0.0) # amount of thermal energy can't be <0
